a5b3ff2009e582e3170919994e8517b53d599e4f
Tomasz Bakula <tbakula92@gmail.com>
2024-12-11 22:40:51 +0100

    Import and apply 'Inter' and 'Space Grotesk' fonts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="text-center text-2xl font-bold text-white">
-      Welcome to DevFlow
-    </h1>
+    <>
+      <h1 className="text-center text-2xl font-bold">Welcome to DevFlow</h1>
+      <h1 className="text-center font-space-grotesk text-2xl font-bold">
+        Welcome to DevFlow
+      </h1>
+    </>
   );
 }
